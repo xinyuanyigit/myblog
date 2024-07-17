ecf0e3f129e18ba76d82cc71ba44bd4991598917
xinyuanyi <1546122187@qq.com>
2024-07-17 15:31:32 +0800

    添加分页请求、登录用户视图、通用常量、权限校验AOP、SQL工具、网络工具、MyBatis Plus配置和全局跨域配置

---
+++ src/main/java/com/wkh/project/common/DeleteRequest.java
@@ -0,0 +1,21 @@
+package com.wkh.project.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 删除请求
+ *
+ * @author wangkehua
+ */
+@Data
+public class DeleteRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
